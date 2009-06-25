6f46ae916a94719dd27abb9f47bebeeb3f88cb95
Katateochi <katateochi@Kettaku.(none)>
2009-06-26 00:14:37 +0100

    stylez and starting to add more functionality

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,22 @@ class Project < ActiveRecord::Base
   def next_num
     [0,self.tickets.map{|t| t.number}].flatten.compact.max + 1
   end
+
+
+  def active_tickets
+    self.tickets.find(:all, :conditions => {:active => true, :on_hold => false, :invalid => false})
+  end
+
+  def closed_tickets
+    self.tickets.find(:all, :conditions => {:active => false})
+  end
+
+  def on_hold_tickets
+    self.tickets.find(:all, :conditions => {:active => true, :on_hold => true})
+  end
+
+  def invalid_tickets
+    self.tickets.find(:all, :conditions => {:active => true, :invalid => true})
+  end
+
 end

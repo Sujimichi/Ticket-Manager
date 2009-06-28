95d61e0cfdcd976b517c6bf2541cc9905fbbe733
katateochi <katateochi@eki.(none)>
2009-06-28 02:43:56 +0100

    added tickets on front page, added new project on front page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,10 @@ class WelcomeController < ApplicationController
 
 
   def index
+    if current_user
+      @active_projects = current_user.projects.active
+      @latest_tickets = current_user.tickets.active.find(:all, :limit => 5, :order => "created_at DESC")
+    end
   end
 
 end
